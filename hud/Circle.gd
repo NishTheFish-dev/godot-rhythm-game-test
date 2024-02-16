cf75ae9006db149f7318ce097328a5dd9c41ee8f
NishTheFish <gamesrekt@gmail.com>
2024-02-15 21:44:28 -0700

    adding basic background

---
+++ hud/Circle.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	queue_redraw()
+
+
+func _draw():
+	draw_circle(Vector2(960, 540), 500, Color( 0.75, 0.75, 0.75, 1 ))
+	draw_circle(Vector2(960, 540), 490, Color( 0.07, 0.07, 0.07, 1 ))
