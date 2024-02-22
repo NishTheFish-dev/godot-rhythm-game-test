9ccd24c286acb65f8940ac64619dcccfb5bce69a
NishTheFish <gamesrekt@gmail.com>
2024-02-21 23:03:19 -0700

    notemap WIP

--- Notemap/Notemap.gd
+++ Notemap/Notemap.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 signal notePlacement(position)
 signal noteBeatmap(beatPos)
+signal drawNote
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	place_Note("three", 4)
 	place_Note("one", 4)
 
 
@@ -13,6 +15,7 @@ func _process(delta):
 	pass
 
 func place_Note(position, beatPos):
+	drawNote.emit()
 	notePlacement.emit(position)
 	noteBeatmap.emit(beatPos)
 	pass
