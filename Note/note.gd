e908f1dc329f888a3bd6f7a348c50983e5533a47
NishTheFish <gamesrekt@gmail.com>
2024-02-22 16:11:50 -0700

    too much testing im ripping my eyes out

--- Note/note.gd
+++ Note/note.gd
@@ -1,22 +1,18 @@
 extends Node2D
-
-var note
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-#func _on_notemap_draw_note():
+#
+#var note
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
+#
+#
+#func _on_notemap_create_note():
 	#var note = Node2D.new()
 	#add_child(note)
 	#pass # Replace with function body.
-
-
-func _on_note_position_draw():
-	pass # Replace with function body.
--- Notemap/Notemap.gd
+++ Notemap/Notemap.gd
@@ -3,19 +3,28 @@ extends Node2D
 signal notePlacement(position)
 signal noteBeatmap(beatPos)
 signal drawNote
+signal createNote
+
+var notePlaceAndColor: Array:
+	set(placement):
+		notePlaceAndColor = placement
+		queue_redraw()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	place_Note("three", 4)
-	place_Note("one", 4)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _draw():
+	place_Note("one", 4)
+	place_Note("three", 4)
 
 func place_Note(position, beatPos):
-	drawNote.emit()
+	createNote.emit()
 	notePlacement.emit(position)
 	noteBeatmap.emit(beatPos)
-	pass
+	#drawNote.emit()
