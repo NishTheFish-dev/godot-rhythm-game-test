f55e70200c23e099a7e9ad52c613c111138e09a2
NishTheFish <gamesrekt@gmail.com>
2024-02-22 18:19:26 -0700

    make it end
    blease

--- Note/TESTNote.gd
+++ Note/TESTNote.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var note
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_notemap_create_note():
+	var note = Node2D.new()
+	add_child(note)
+	pass # Replace with function body.
